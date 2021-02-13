f641ac41b4fa5cbda3d1549eb15c2eb50b88740e
hernani almeida <her_almeida23@hotmail.com>
2021-02-13 10:50:27 -0300

    pagina home, pagina carrinho concluida e api funcional buscando dados em products.json

--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -1,10 +1,61 @@
 import React from 'react';
-
+import { Container, TabelaJogo, Total} from './styles';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
 
 export default function Carrinho() {
 
   return (
-<div>Vamos iniciar</div>
+<Container>
+  <TabelaJogo>
+     <thead>
+       <tr>
+         <th />
+         <th>JOGO</th>
+         <th>QTD</th>
+         <th>SUBTOTAL</th>
+         <th />
+       </tr>
+     </thead>
+     <tbody>
+       <tr>
+         <td>
+           <img src="" alt="jogo" />
+         </td>
+         <td>
+           <strong>Jogo legal</strong>
+           <span>R$100</span>
+         </td>
+         <td>
+           <div>
+           <button type="button">
+             <MdRemoveCircleOutline size={20} color="0000" />
+           </button>
+          <input type="number" readOnly value={1} />
+           <button type="button">
+             <MdAddCircleOutline size={20} color="0000" />
+           </button>
+           </div>
+         </td>
+         <td>
+           <strong>R$ 200</strong>
+         </td>
+         <td>
+         <button type="button">
+             <MdDelete size={20} color="0000" />
+           </button>
+         </td>
+       </tr>
+     </tbody>
+  </TabelaJogo>
+  <footer>
+    <button type="button"> Finalizar Pedido</button>
+
+    <Total>
+      <span>TOTAL</span>
+      <strong>R$ 1900</strong>
+    </Total>
+  </footer>
+</Container>
 
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { GamesList } from './styles'
+import api from '../../services/api'
+
 
 
 export default function Home() {
+  const [jogos, setJogos] = useState([]);
 
-  return (
-<div>Vamos iniciar</div>
+  useEffect(() => {
+    
+    async function loadGames(){
+    const response = await api.get('products.json')
+    setJogos(response.data)
+  }
+  loadGames()
+  }, []);
+  return (console.log(jogos),
+    <GamesList>
+    {jogos.map(jogo => (
+      <li key={jogo.id}>
+         <img 
+         src ={jogo.image}
+         alt = {jogo.name}
+         />
+         <strong>{jogo.name}</strong>
+         <span> R$ {jogo.price}</span>
+         <span>SCORE: {jogo.score}</span>
+         <button type="button">
+           <div>
+             <MdAddShoppingCart size={16} color="#FFF" />
+           </div>
+           <span>ADICIONAR AO CARRINHO</span>
+         </button>
+      </li>
+    ))}  
+    </GamesList>
 
   );
 }
