145b5aa79abee8f780269a62b2397b59c9f45784
hernani almeida <her_almeida23@hotmail.com>
2021-02-12 23:01:04 -0300

    adicionando dependencias e estruturando rotas e paginas

---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdShoppingBasket } from 'react-icons/md';
+import { Container, Cart } from './styles';
+
+
+
+function Header() {
+
+  
+  
+  return (
+    <Container>
+      <Link to="/">
+        <img src= "" alt= "logo" />
+      </Link>
+      
+
+      <Cart to="/carrinho">
+        <div>
+          <strong>Meu carrinho</strong>
+          <span>3 itens</span>
+          
+        </div>
+        <MdShoppingBasket size={36} color="#FFF" />
+      </Cart>
+    </Container>
+  );
+}
+
+
+export default Header;
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Carrinho from './pages/Carrinho';
+export default function App() {
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/carrinho" component={Carrinho} />
+    </Switch>
+  );
+}
